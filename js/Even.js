170762d218d923a8e3133335aae9974066e8f4d1
Anish S Nair <anish.vilayil.s@gmail.com>
2019-03-27 09:38:53 +0530

    JS

---
+++ js/Even.js
@@ -0,0 +1,10 @@
+var num1=101;
+
+if(num1%2==0){
+    console.log("even number");
+}
+else{
+
+    console.log("Odd number");
+  
+}
---
+++ js/greatestnum.js
@@ -0,0 +1,31 @@
+var num1=100;
+var num2=234;
+var num3=7567;
+
+
+if(num1>num2)
+{
+
+    if(num1>num3){
+        console.log(num1);
+
+    }
+    else{
+        console.log(num3);
+
+    }
+}
+
+else{
+
+    if(num2>num3)
+    {
+        console.log(num2);
+
+    }
+    else{
+        console.log(num3);
+
+    }
+
+}
